920b5c204569fd0736d7f9e782faef92d94d624d
Zhiyuan Zhou <zhou.zhiyuan.stephen@hotmail.com>
2020-09-27 22:47:21 -0700

    Initial Commit

---
+++ LAApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LAApp
+//
+//  Created by Zhou Zhiyuan on 9/27/20.
+//  Copyright © 2020 Zhou Zhiyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
